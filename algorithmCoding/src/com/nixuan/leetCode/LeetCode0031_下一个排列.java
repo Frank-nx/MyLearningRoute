989acf9628e71243990a9c5901f3add5dbb53608
nixuan <nixuanwb@163.com>
2018-10-26 11:03:14 +0800

    更新算法题

--- algorithmCoding/src/com/nixuan/leetCode/LeetCode0031_下一个排列.java
+++ algorithmCoding/src/com/nixuan/leetCode/LeetCode0031_下一个排列.java
@@ -9,7 +9,33 @@ package com.nixuan.leetCode;
 public class LeetCode0031_下一个排列 {
 
     public static void nextPermutation(int[] nums) {
+        if(nums == null || nums.length < 1){
+            return;
+        }
+        int index = nums.length - 2;
+        int right = nums.length - 1;
+        while(index >=0 && nums[index] >= nums[index+1]){
+            index--;
+        }
+        if(index >= 0){
+            while(right > 0 && nums[right] <= nums[index]){
+                right--;
+            }
+            swap(nums,index,right);
+        }
+        reverse(nums,index+1,nums.length-1);
+    }
 
+    public static void reverse(int[] nums,int left,int right){
+        while(left < right){
+            swap(nums,left++,right--);
+        }
+    }
+
+    public static void swap(int[] nums,int left,int right){
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
     }
 
 }
---
+++ algorithmCoding/src/com/nixuan/leetCode/LeetCode0033_搜索旋转排序数组.java
@@ -0,0 +1,43 @@
+package com.nixuan.leetCode;
+
+/**
+ * @program: MyLearningRoute
+ * @description:
+ * @author: nixuan
+ * @create: 2018-10-26 10:37
+ **/
+public class LeetCode0033_搜索旋转排序数组 {
+
+    public static void main(String[] args) {
+        int[] arr = {4,5,6,7,0,1,2};
+        System.out.println(search(arr,0));
+    }
+
+    public static int search(int[] nums, int target) {
+        if(nums == null || nums.length < 1){
+            return -1;
+        }
+        int left = 0;
+        int right = nums.length - 1;
+        while(left < right){
+            int mid = ((right - left) >> 1) + left;
+            if(nums[mid] == target){
+                return mid;
+            }
+            if(nums[mid] >= nums[left]){
+                if(nums[left] > target){
+                    right = mid - 1;
+                }else{
+                    left = mid + 1;
+                }
+            }else{
+                if(nums[mid] < target){
+                    left = mid + 1;
+                }else{
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
